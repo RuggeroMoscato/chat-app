d1e468d984983bf9915a09ff14a64b0483e96c43
RuggeroMoscato <ruggeromoscato04@gmail.com>
2024-05-23 12:56:15 +0200

    general fixes and server implements

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -14,9 +14,9 @@ export default function Welcome() {
     <Container>
       <img src={Robot} alt="" />
       <h1>
-        Welcome, <span>{userName}!</span>
+        Benvenuto, <span>{userName}!</span>
       </h1>
-      <h3>Please select a chat to Start messaging.</h3>
+      <h3>Seleziona una chat per iniziare a scrivere.</h3>
     </Container>
   );
 }
