9c9f65a258c4bd0cf31e85b8967ce2933304e79a
RuggeroMoscato <ruggeromoscato04@gmail.com>
2024-07-05 09:13:02 +0200

    small fix

--- public/src/components/Logout.jsx
+++ public/src/components/Logout.jsx
@@ -29,7 +29,6 @@ const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0.5rem;
   border-radius: 0.5rem;
   background-color: #9a86f3;
   border: none;
